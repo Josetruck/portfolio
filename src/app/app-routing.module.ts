f99a35c7037c249e8749835023c0836fb17612c0
Josetruck <olemiluis@gmail.com>
2023-02-15 11:49:09 +0100

    restaurapp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RestaurappComponent } from './restaurapp/restaurapp.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/restaurapp', pathMatch: 'full' },
+  { path: 'restaurapp', component: RestaurappComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/restaurapp/restaurapp.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurapp',
+  templateUrl: './restaurapp.component.html',
+})
+export class RestaurappComponent {
+
+}
